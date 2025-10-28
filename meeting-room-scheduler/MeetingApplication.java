ebadebcc82ab90d19770532d5e244a6600637e7e
himanshisingh23 <himanshisingh23>
2025-10-28 17:13:15 +0530

    added Calendar and TimeSlot Entities

--- meeting-room-scheduler/MeetingApplication.java
+++ meeting-room-scheduler/MeetingApplication.java
@@ -73,8 +73,50 @@ class MeetingRoom {
 }
 
 // TimeSlot
+class TimeSlot {
+    int startTime; 
+    int endTime;
+
+    public TimeSlot(int startTime, int endTime){
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public boolean isOverlapping (TimeSlot slot, TimeSlot newSlot){
+        if((slot.startTime <= newSlot.startTime && slot.endTime <= newSlot.startTime) || 
+        (slot.startTime >= newSlot.endTime && slot.endTime >= newSlot.endTime)){
+            return true;
+        }
+        return false;
+    }
+}
 
 // Calendar
+class Calendar {
+    List<TimeSlot>bookings;
+
+    public Calendar (){
+        this.bookings = new ArrayList<>();
+    }
+
+    public void addTimeSlot (TimeSlot timeSlot){
+        this.bookings.add(timeSlot);
+    }
+
+    public void removeTimeSlot(TimeSlot timeSlot){
+        this.bookings.remove(timeSlot);
+    }
+
+    public boolean isAvailable (){
+        if(bookings.size()==0)return true;
+        for(TimeSlot booking : this.bookings){
+            if(slot.isOverlapping(slot, booking)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
 
 // Location
 class Location{

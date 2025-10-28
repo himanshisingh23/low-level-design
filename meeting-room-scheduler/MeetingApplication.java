922a6e0ae976311b64fee6ac92d109124719b307
himanshisingh23 <himanshisingh23>
2025-10-28 19:42:42 +0530

    added main function

--- meeting-room-scheduler/MeetingApplication.java
+++ meeting-room-scheduler/MeetingApplication.java
@@ -10,7 +10,7 @@ Class Participant implements Observer {
     Calendar calendar;
 
     public Participant (String name, String email, String mobile){
-        this.name = name;/
+        this.name = name;
         this.email = email;
         this.mobile = mobile;
         this.calendar = new Calendar();
@@ -129,3 +129,40 @@ class Location{
     }
 }
 
+// Room Selection Strategy
+
+
+// Meeting Room Controller
+
+
+// Meeting Scheduler
+
+
+// MeetingApplication
+class MeetingApplication {
+    public static void main (String[] args){
+        MeetingRoom room1 = new MeetingRoom("Room1", 3, new Location(1,101));
+        MeetingRoom room2 = new MeetingRoom("Room2", 4, new Location(1,101));        
+        MeetingRoom room3 = new MeetingRoom("Room3", 3, new Location(1,101));
+
+        MeetingRoomController roomController = new MeetingRoomController();
+
+        roomController.addMeetingRoom(room1);    
+        roomController.addMeetingRoom(room2);
+        roomController.addMeetingRoom(room3);
+
+        Participant participant1 = new Participant("ABC", "abc@gmail.com". "+0886756587");
+        Participant participant2 = new Participant("PQR", "pqr@gmail.com". "+0236876587");
+        Participant participant3 = new Participant("XYZ", "xyz@gmail.com". "+0886756337");
+
+        List<Participant> participants = new ArrayList<>();
+        participants.add(participant1);
+        participants.add(participant2);
+        participants.add(participant3);
+
+        MeetingScheduler scheduler = new MeetingScheduler(new FirstComeFirstServeStrategy(), roomController);
+        scheduler.scheduleMeeting("M1", "Archiving Discussion", participants, new TimeSlot(2,3));
+        scheduler.scheduleMeeting("M1", "Archiving Discussion", participants, new TimeSlot(3,4));
+        
+    }
+}
